2bd73c031cd777cc62069069f036afbf2cbe2f09
dsrkafuu <dsrkafuu@outlook.com>
2022-05-18 14:10:12 +0800

    2022-05-18T06:10:12.482Z

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -54,6 +54,7 @@ const template = `<!DOCTYPE html>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>DSRAssets</title>
+<link rel="icon" sizes="any" href="/favicon.ico" />
 </head>
 <body>
 {{inject}}
